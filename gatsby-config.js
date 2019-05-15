90d2ef468932b4624bdf8ead7cf1850b6772abec
McSteve7 <stephenajiboye7@gmail.com>
2019-05-15 06:18:32 +0100

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 const config = require('./config/website')
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+const pathPrefix = config.pathPrefix === '/gatsby-portfolio-site' ? '' : config.pathPrefix
 
 module.exports = {
   /* General Information */
