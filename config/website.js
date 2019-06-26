ede7d6dc2eedadff6559ab31b6ddc99505781bea
McSteve7 <stephenajiboye7@gmail.com>
2019-06-26 10:21:08 +0100

    favicon replacement

--- config/website.js
+++ config/website.js
@@ -1,7 +1,7 @@
 const tailwind = require('../tailwind')
 
 module.exports = {
-  pathPrefix: '/McSteve7', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
   siteTitle: 'Damilola Ajiboye - Front-end web developer', // Navigation and Site Title
   siteTitleAlt: 'Damilola Ajiboye - Front-end web developer', // Alternative Site title for SEO
@@ -9,7 +9,7 @@ module.exports = {
   siteHeadline: 'Online Portfolio of Damilola Ajiboye', // Headline for schema.org JSONLD
   siteUrl: 'https://github.com/McSteve7/gatsby-portfolio-site.git', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logo.png', // Used for SEO and manifest
+  siteLogo: 'favicons/avatar.jpg', // Used for SEO and manifest
   siteDescription: 'The online portfolio of Damilola Ajiboye, A web developer based in Lagos Nigeria.',
   author: 'Damilola Ajiboye', // Author for schema.org JSONLD
 
--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -25,7 +25,7 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
   }
   button{
-    font-size: 30px;
+    font-size: 20px;
     border-radius: 5px;
   }
   button:hover{
